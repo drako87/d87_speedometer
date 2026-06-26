733e8c25c7cbaa5c13e1a18bba8780544c41684e
Drako87 <drakogamer87@gmail.com>
2026-06-26 19:07:18 +0200

    Update fxmanifest with ox_lib initialization
    Added initialization scripts for ox_lib and updated author description.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,13 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Drako87/Dracatt'
-description 'd87-speedometer - Velocímetro minimalista premium multiframework con Checker de versiones'
+description 'D87 Speedometer - Velocímetro minimalista premium multiframework con Checker de versiones'
 version '1.0.0'
 
--- Registramos los scripts compartidos, de cliente y el NUEVO de servidor
+-- IMPORTANTE: Cargamos los scripts de inicialización de ox_lib para Cliente y Servidor
+shared_script '@ox_lib/init.lua'
+
+-- Registramos la estructura de scripts del recurso
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
